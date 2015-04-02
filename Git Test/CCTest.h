32a8682b27157380eb362ffd903aa0599c848c99
MacDevVm <MacDevVM@MacDevVms-Mac.local>
2015-04-02 10:19:11 -0700

    added test to class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by MacDevVm on 4/2/15.
+//  Copyright (c) 2015 Group H Network. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by MacDevVm on 4/2/15.
+//  Copyright (c) 2015 Group H Network. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
